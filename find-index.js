826a8bf0aceddc63f5b277286ab379ab181579fd
ssan9 <perrsy@gmail.com>
2018-08-16 14:27:56 -0400

    added coding challenges

---
+++ find-index.js
@@ -0,0 +1,16 @@
+// Find the Index
+// Create a function that takes an array and a string as arguments and return the index of the string.
+// Examples
+// ['hi', 'edabit', 'fgh', 'abc'], 'fgh' ➞ 2
+
+// ['Red', 'blue', 'Blue', 'Green'], 'blue' ➞ 1
+
+// ['a', 'g', 'y', 'd'], 'd' ➞ 3
+
+// ['Pineapple', 'Orange', 'Grape', 'Apple'], 'Pineapple' ➞ 0
+
+
+function find_index(arr, str) {
+	return arr.indexOf(str);
+}
+	
---
+++ num-less-than-or-equal-to-0.js
@@ -0,0 +1,14 @@
+// Is the Number Less than or Equal to Zero?
+// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+// Examples
+// 5 ➞ false
+
+// 0 ➞ true
+
+// -2 ➞ true
+
+// 1 ➞ false
+
+function lessThanOrEqualToZero(num) {
+	return num <= 0;
+}
